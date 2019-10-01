3527b69052a8696faee39dc642577228e8281310
Anis Madaki <Anis.Madaki@3ds.com>
2019-10-01 19:42:09 +0530

    store middleware and router

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,64 @@
 import React from 'react';
-const StreamCreate = () => {
-     return <div>Stream Create</div>;
- }
- export default StreamCreate;
+import {Field, reduxForm} from 'redux-form'; 
+import {connect} from 'react-redux';
+import {createStream} from '../../actions';
+class StreamCreate extends React.Component{
+    renderError({error, touched}){
+        if(touched && error){
+            return(
+                <div className='ui error message'>
+                    <div className='header'>
+                        {error}
+                    </div>
+                </div>
+            )
+        }
+    }
+    renderInput = ({input, label, meta}) => {
+        //console.log(meta)
+        const className = `field ${meta.error && meta.touched ? 'error' : ''} `;
+        return (
+            <div className = {className}>
+                <label>{label}</label>
+                <input {...input}/>
+                {this.renderError(meta)}
+                
+            </div>
+            )
+    }
+    onSubmit = (formValues)=>{
+        //event.preventDefault();
+        console.log(formValues)
+        this.props.createStream(formValues);
+    }
+    render(){
+        //console.log(this.props)
+        return (
+            <div>
+                <form className ='ui form error' onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                    <Field name = 'title' component={this.renderInput} label ='Enter Title'/>
+                    <Field name = 'description' component={this.renderInput} label ='Enter Description'/>
+                    <button className='ui button primary'>Submit</button>
+                </form>                    
+            </div>
+        );
+    }   
+}
+
+const validate =(formValues) => {
+    const error= {};
+    if(!formValues.title){
+        error.title = 'You must enter title';
+    }
+    if(!formValues.description){
+        error.description = 'You must enter description';
+    }
+    return error;
+}
+
+const formWrapped = reduxForm({
+    form:'streamCreate',
+    validate : validate
+})(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped)
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,32 @@
 import React from 'react';
-const StreamList = () => {
-     return <div>Stream List</div>;
+import {connect} from 'react-redux';
+import {fetchStreams} from '../../actions';
+class StreamList extends React.Component  {
+    componentDidMount(){
+        this.props.fetchStreams();
+    }
+    renderList(){
+        return this.props.streams.map((stream)=>{
+            return (
+                <div className = 'item' key={stream.id}>
+                    <i className = 'large middle aligned icon camera'/>
+                    <div className='content'>{stream.title}</div>
+                    <div className='description' >{stream.desscription}</div>
+                </div>                    
+            )
+        })
+    }
+    render(){ 
+        console.log(this.props.streams);
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className ='ui celled list'>{this.renderList()}</div>
+            </div>
+        );
+    }
  }
- export default StreamList;
+ const mapStateToProps = (state) =>{
+     return {streams : Object.values(state.streams)}
+ }
+ export default connect(mapStateToProps, {fetchStreams})(StreamList);
